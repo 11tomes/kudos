08f1a6ded178e27903d0d33589486ecea0a97dd6
Dixie Philamerah Atay <dixie.atay@gmail.com>
2020-12-18 15:33:37 +0800

    Kudos init setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Kudos - Employee and Engagement System</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="author" content="SoonLaunch">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <!-- Place favicon.ico in the root directory -->
+
+		<!-- ========================= CSS here ========================= -->
+		<link rel="stylesheet" href="assets/css/bootstrap-4.5.0.min.css">
+        <link rel="stylesheet" href="assets/css/lineicons.css">
+		<link rel="stylesheet" href="assets/css/animate.css">
+		<link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- ========================= main start ========================= -->
+        <main class="main-03">
+            <!-- shapes start  -->
+            <div class="shape shape-1"></div>
+            <img src="assets/img/shape-1.svg" alt="" class="shape shape-3">
+            <img src="assets/img/shape-2.svg" alt="" class="shape shape-4">
+            <img src="assets/img/shape-3.svg" alt="" class="shape shape-5">
+            <img src="assets/img/shape-4.svg" alt="" class="shape shape-6">
+            <img src="assets/img/shape-5.svg" alt="" class="shape shape-7">
+            <img src="assets/img/shape-6.svg" alt="" class="shape shape-8">
+            <img src="assets/img/shape-7.svg" alt="" class="shape shape-9">
+            <img src="assets/img/shape-8.svg" alt="" class="shape shape-10">
+            <img src="assets/img/shape-9.svg" alt="" class="shape shape-11">
+            <img src="assets/img/shape-10.svg" alt="" class="shape shape-12">
+            <img src="assets/img/shape-11.svg" alt="" class="shape shape-13">
+            <!-- shapes end  -->
+            <!-- header start  -->
+            <div class="header header-03">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <a href="#" class="logo"> <img src="assets/img/logo-2.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- header end  -->
+            <!-- ========================= main-wrapper start ========================= -->
+            <div class="main-wrapper demo-03">
+                <div class="shape shape-2"></div>
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-6 col-lg-6 col-md-12">
+                            <!-- content-wrapper start  -->
+                            <div class="content-wrapper">
+                                <h1 class="wow fadeInUp" data-wow-delay=".2s">Engage. Appreciate.<br> Give Kudos!</h1>
+                                <p class="wow fadeInLeft" data-wow-delay=".4s">Spark joy in your workplace, inspire, and be inspired. This is what we want you to get, this is what "Kudos" gives...</p>
+        
+                                <div
+                                    class="subscribe-form subscribe-form-1 validate wow fadeInUp" data-wow-delay=".7s"
+                                    target="_blank" novalidate>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input
+                                            type="text" name="b_4dbefd9d3a1d6a570020b1e85_e16d098ae8" tabindex="-1"
+                                            value=""></div>
+
+                                    <button onclick='window.location.href="{{ route('register') }}"' id="subscribe" class="sub_btn common-btn">Engage now!</button>
+                                </div>
+                            </div>
+                            <!-- content-wrapper end  -->
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-6">
+                            <!-- img-wrapper start  -->
+                            <div class="img-wrapper wow fadeInRight" data-wow-delay=".5s">
+                                <img src="assets/img/ballons.svg" alt="">
+                            </div>
+                            <!-- img-wrapper end  -->
+                        </div>
+                    </div> <!-- end row  -->
+                </div> <!-- end container  -->
+            </div>
+            <!-- ========================= main-wrapper end ========================= -->
+            <p class="text-center"></p>
+        </main>
+        <!-- ========================= main end ========================= -->
+        
+		<!-- ========================= JS here ========================= -->
+		<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+		<script src="assets/js/vendor/jquery-3.5.1.min.js"></script>
+		<script src="assets/js/bootstrap-4.5.0.min.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+		<script src="assets/js/main.js"></script>
+    </body>
+</html>
