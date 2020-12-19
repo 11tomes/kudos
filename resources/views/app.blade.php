b43570268955ba7496c9c9b3790b8ff8ea3f1643
Leonel Tomes <leonel.tomes@codingavenue.com>
2020-12-20 04:07:09 +0800

    NProgress.js

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,22 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+        <style>
+        #nprogress .bar {
+            background: #FFBB00 !important;
+        }
+
+        #nprogress .peg {
+            box-shadow: 0 0 10px #FFBB00, 0 0 5px #FFBB00;
+        }
+
+        #nprogress .spinner-icon {
+            border-top-color: #FFBB00;
+            border-left-color: #FFBB00;
+        }
+        </style>
+
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
